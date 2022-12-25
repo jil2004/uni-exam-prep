4fa3e1cf4ab7d866c96edf6a53893eb2525deb3b
jil2004 <110085243+jil2004@users.noreply.github.com>
2022-12-25 16:56:46 +0530

    Update perfect_num.c

--- perfect_num.c
+++ perfect_num.c
@@ -1,3 +1,14 @@
+/* Definition of perfect number or What is perfect number? 
+
+Perfect number is a positive number which sum of all positive divisors excluding that number is equal to that number. For example 6 is perfect number since divisor of 6 are 1, 2 and 3.  Sum of its divisor is
+1 + 2+ 3 =6
+
+Note: 6 is the smallest perfect number.
+
+Next perfect number is 28 since 1+ 2 + 4 + 7 + 14 = 28
+Some more perfect numbers: 496, 8128
+  */
+
 
 
 // Code 1:
